59bc1217ffdb80383ae2fdc249d70c3bc1d5f55b
tc9011 <tc9011@126.com>
2020-04-21 17:36:03 +0800

    feat: add vue create setting

--- packages/leo/cli/commands/vue.js
+++ packages/leo/cli/commands/vue.js
@@ -1,9 +1,23 @@
+const chalk = require('chalk')
+const { failSpinner } = require('../utils/spinner')
+const { stopSpinner } = require('../utils/spinner')
+const { logWithSpinner } = require('../utils/spinner')
+const { clearConsole } = require('../utils/logger')
 const { run } = require('../utils/runCommand')
 const { hasCommand } = require('../utils/confirmCommand')
 
 async function initVue (projectName) {
   if (!hasCommand('vue --version')) {
     await run('npm', ['install', '@vue/cli', '-g'])
+  }
+
+  await clearConsole()
+  logWithSpinner(`Creating an Vue project by ${chalk.yellow('leo')} . \n`)
+  try {
+    await run('vue', ['create', '--preset', 'leo-tools/vue-cli-preset', projectName])
+    stopSpinner()
+  } catch (e) {
+    failSpinner(chalk.red(e.stderr))
   }
 }
 
