e01e85e7d7991262e945f7050dd747c5c03256d7
tc9011 <tc9011@126.com>
2020-04-22 12:05:11 +0800

    feat: add default setting for vue project

--- packages/leo/cli/commands/vue.js
+++ packages/leo/cli/commands/vue.js
@@ -1,4 +1,5 @@
 const chalk = require('chalk')
+const inquirer = require('inquirer')
 const { failSpinner } = require('../utils/spinner')
 const { stopSpinner } = require('../utils/spinner')
 const { logWithSpinner } = require('../utils/spinner')
@@ -11,10 +12,23 @@ async function initVue (projectName) {
     await run('npm', ['install', '@vue/cli', '-g'])
   }
 
+  const { isDefault } = await inquirer.prompt([
+    {
+      type: 'confirm',
+      message: ' Would you like to use recommend settings for vue project ?',
+      name: 'isDefault',
+      default: true
+    }
+  ])
+
   await clearConsole()
   logWithSpinner(`Creating an Vue project by ${chalk.yellow('leo')} . \n`)
   try {
-    await run('vue', ['create', '--preset', 'leo-tools/vue-cli-preset', projectName])
+    if (isDefault) {
+      await run('vue', ['create', '--preset', 'leo-tools/vue-cli-preset', projectName])
+    } else {
+      await run('vue', ['create', '--default', projectName])
+    }
     stopSpinner()
   } catch (e) {
     failSpinner(chalk.red(e.stderr))
